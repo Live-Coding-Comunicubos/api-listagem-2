20fa9734ff12f06be6219b1899ea2fa84f887529
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:13:10 -0300

    feat: excluir usuario

--- src/server.js
+++ src/server.js
@@ -25,6 +25,13 @@ app.post("/lista", (req, res)=> {
 })
 
 
+app.delete("/lista/:id", (req, res)=> {
+    const indexUsuario = lista.findIndex((usuario) => usuario.id === id);
+    lista.splice(indexUsuario, 1);
+    return res.status(200).json({ texto:"usuario excluido"})
+})
+
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
