e940316c089b6b777d9c7125c65fdfc6537d1a48
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:03:26 -0300

    feat: array de usuario

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+const lista = [
+    {
+        id: '1',
+        nome: 'amanda',
+    },
+    {
+        id: '2',
+        nome: 'patrick',
+    },
+    {
+        id: '3',
+        nome: 'ygor',
+    },
+]
+
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
