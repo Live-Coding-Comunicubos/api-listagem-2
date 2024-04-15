cc1c8ccdbd969e6778b2fd65c21c41012ae2dcea
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:12:44 -0300

    feat: post de adicionar usuario

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
 
+const lista = [
+    {
+        id: '1',
+        nome: 'amanda',
+    },
+    {
+        id: '2',
+        nome: 'patrick',
+    },
+    {
+        id: '3',
+        nome: 'ygor',
+    },
+]
+
+app.post("/lista", (req, res)=> {
+    lista.push({
+        id: (lista.length) + 2,
+        nome: 'Novo Usuario'
+    })
+    return res.status(201).json({ texto:"novo usuario adicionado"})
+})
+
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
